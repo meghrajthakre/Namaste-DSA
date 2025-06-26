e3336afa2e7a850da79a9ddd6c2f3166d8b4f3b0
meghraj thakre <meghrajthakre444@gmail.com>
2025-06-26 14:48:48 +0530

    moveZeros

--- Arrays/moveZeros.js
+++ Arrays/moveZeros.js
@@ -38,3 +38,11 @@ function moveZeros(arr) {
 
 let res2 = moveZeros([0, 2, 0, 1, 2, 3, 0]);
 console.log(res2);  // ➜ [2, 1, 2, 3, 0, 0, 0]
+
+// x points to where the next non-zero should go.
+
+// i goes through the array.
+
+// Jab non-zero mile, to arr[x] aur arr[i] swap karo (agar alag hai).
+
+// Isse sabhi non-zero left me aa jaate hain, aur zero peeche chale jaate hain — bina 2nd loop ke.
